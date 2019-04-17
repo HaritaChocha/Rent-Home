53d83e15029dd68255eabdd56bb42d2357e64dd9
Harita Chocha <harita.chocha@gmail.com>
2019-04-16 22:08:56 -0400

    Update Coordinates.java

--- RentHome/src/main/java/com/uwindsor/project/models/Coordinates.java
+++ RentHome/src/main/java/com/uwindsor/project/models/Coordinates.java
@@ -10,9 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-/**
- * @author Afnan Ur Rehman
- */
+
 @Entity
 @Table(name = "coordinates")
 public class Coordinates {
