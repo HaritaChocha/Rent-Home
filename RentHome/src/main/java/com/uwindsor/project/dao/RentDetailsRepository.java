272408adf2416287b02a3dbbd3ccf2a2dbd8edfe
Harita Chocha <harita.chocha@gmail.com>
2019-04-16 22:10:39 -0400

    Update RentDetailsRepository.java

--- RentHome/src/main/java/com/uwindsor/project/dao/RentDetailsRepository.java
+++ RentHome/src/main/java/com/uwindsor/project/dao/RentDetailsRepository.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.uwindsor.project.models.Coordinates;
 import com.uwindsor.project.models.RentDetails;
-/**
- * @author Afnan Ur Rehman
- */
+
 @Repository("rentDetailsRepository")
 public interface RentDetailsRepository extends CrudRepository<RentDetails, Integer>{
 
